e7eb783385f0347eb45fb29628343aa958ae3e2f
Ethan Harrasser <ethan.harrasser@gmail.com>
2024-05-17 16:41:08 -0700

    commented out unused imports, db init and migration partially complete

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,4 +1,4 @@
 from .auth import auth_routes
 from .user import user_routes
-from .expenses import expense_routes
-from .friends import friend_routes
+# from .expenses import expense_routes
+# from .friends import friend_routes
