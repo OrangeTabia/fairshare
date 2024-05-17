6d3279227e602616a4e6cf2db17f0ae3715cd607
Tabia Ye <tabiaye@gmail.com>
2024-05-17 13:53:36 -0700

    file setup

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,6 @@
 from .db import db
 from .user import User
 from .db import environment, SCHEMA
+from .comments import Comments
+from .expenses import Expenses
+from .payments import Payments
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,2 +1,4 @@
-from app.routes.auth import auth_routes
-from app.routes.user import user_routes
+from .auth import auth_routes
+from .user import user_routes
+from .expenses import expense_routes
+from .friends import friend_routes
