69bff2c7f4a8fa113b9134534ce805225265955e
Tabia Ye <tabiaye@gmail.com>
2024-05-17 10:38:41 -0700

    folder set up

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,11 @@ from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
 from .models import db, User
-from .api.user_routes import user_routes
-from .api.auth_routes import auth_routes
+from .routes import user_routes, auth_routes
 from .seeds import seed_commands
 from .config import Config
 
-app = Flask(__name__, static_folder='../react-vite/dist', static_url_path='/')
+app = Flask(__name__, static_folder='../frontend/dist', static_url_path='/')
 
 # Setup login manager
 login = LoginManager(app)
