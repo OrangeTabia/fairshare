9762cf7e442658dc6d0656acaf343edd1277c121
Tabia Ye <tabiaye@gmail.com>
2024-05-20 13:53:33 -0700

    fixed return all friends

--- app/routes/friends.py
+++ app/routes/friends.py
@@ -20,8 +20,8 @@ def friends_list():
     ]
 
     # sorted(friends_info, key=lambda friend: friend.to_dict()["name"])
-
-    return jsonify([friend.to_dict() for friend in friends_info])
+    # return jsonify([friend.to_dict() for friend in friends_info])
+    return [friend.to_dict() for friend in friends_info]
 
 
 @friends_routes.route("/add_friend", methods=["GET", "POST"])
