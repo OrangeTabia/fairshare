af9d9852c059cc7c774334a4f63bd4a854916a7b
Tabia Ye <tabiaye@gmail.com>
2024-05-21 12:57:40 -0700

    payments routes fixed

--- app/forms/expense_forms/create_expense.py
+++ app/forms/expense_forms/create_expense.py
@@ -7,7 +7,7 @@ class CreateFriendsExpenseForm(FlaskForm):
     payer_id = IntegerField("payer_id", validators=[DataRequired()])
     receiver_id = IntegerField("receiver_id", validators=[DataRequired()])
     description = StringField("description", validators=[Length(max=1000)])
-    amount = IntegerField("amount", validators=[DataRequired(), NumberRange(min=0.01)])
+    amount = IntegerField("amount", validators=[DataRequired(), NumberRange(min=1)])
     expense_date = StringField("expense_date", validators=[DataRequired()])
     settled = BooleanField("settled", false_values={False, "false", ""})
     notes = StringField("notes", validators=[Length(max=200)])
--- app/forms/payments_forms/create_payment.py
+++ app/forms/payments_forms/create_payment.py
@@ -12,6 +12,8 @@ def check_payment_amount(form, field):
     amount = field.data
 
     expense = FriendsExpense.query.get(expense_id)
+    if not expense: 
+        raise ValidationError("Expense do not exist")
     payments = expense.payments
 
     for payment in payments:
@@ -31,6 +33,6 @@ class AddPaymentForm(FlaskForm):
     friends_expense_id = IntegerField("friends_expense_id", validators=[DataRequired()])
     amount = IntegerField(
         "amount",
-        validators=[DataRequired(), NumberRange(min=0.01), check_payment_amount],
+        validators=[DataRequired(), NumberRange(min=1), check_payment_amount],
     )
     payment_date = StringField("payment_date", validators=[DataRequired()])
