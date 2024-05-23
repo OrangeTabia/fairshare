0feff0ae16e213e9250806952a86b98b1a83b881
Jeramie <forbesjeramie@gmail.com>
2024-05-23 16:42:08 -0400

    update middle-modal

--- frontend/src/components/SecondaryNavigation/SecondaryNavigation.jsx
+++ frontend/src/components/SecondaryNavigation/SecondaryNavigation.jsx
@@ -1,29 +1,29 @@
 import "./SecondaryNavigation.css";
 import OpenModalButton from "../modals/OpenModalButton/OpenModalButton";
 import AddExpenseModal from "../modals/AddExpenseModal/AddExpenseModal";
-import SettleUpModal from "../modals/SettleUpModal/SettleUpModal"; 
+import SettleUpModal from "../modals/SettleUpModal/SettleUpModal";
 
 function SecondaryNavigation({ pageTitle }) {
-    return (
-        <header id="secondary-nav-header">
-            <h1 id="page-title">{pageTitle}</h1>
-            <nav>
-                <OpenModalButton
-                buttonText="Add Expense"
-                modalComponent={<AddExpenseModal />}
-                id="add-expense-modal-button"
-                className="modal-button"
-                />
+  return (
+    <header id="secondary-nav-header">
+      <h1 id="page-title">{pageTitle}</h1>
+      <nav>
+        <OpenModalButton
+          buttonText="Add Expense"
+          modalComponent={<AddExpenseModal />}
+          id="add-expense-modal-button"
+          className="modal-button"
+        />
 
-                <OpenModalButton
-                buttonText="Settle Up"
-                modalComponent={<SettleUpModal />}
-                id="settle-up-modal-button"
-                className="modal-button"
-                />
-            </nav>
-        </header>
-    )
+        <OpenModalButton
+          buttonText="Settle Up"
+          modalComponent={<SettleUpModal />}
+          id="settle-up-modal-button"
+          className="modal-button"
+        />
+      </nav>
+    </header>
+  );
 }
 
 export default SecondaryNavigation;
