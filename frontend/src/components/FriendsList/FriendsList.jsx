f143b5a80b9b2a6509fe418a8188d02a1df1865a
Aubrie <aubriewoodbine@yahoo.com>
2024-05-23 14:24:42 -0500

    Add and delete friends

--- frontend/src/components/FriendsList/FriendsList.jsx
+++ frontend/src/components/FriendsList/FriendsList.jsx
@@ -6,25 +6,30 @@ import { useSelector } from "react-redux";
 import './FriendsList.css'
 
 import { HiOutlineX } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 
 
 function FriendsList() {
     const friends = useSelector(state => state.friends)
+    const navigate = useNavigate()
+    const [orderedFriends, setOrderedFriends] = useState([])
 
+    useEffect(() => {
+        const sortedFriends = Object.values(friends).sort((friend1, friend2) => friend1.name > friend2.name ? 1 : friend1.name < friend2.name ? -1 : 0)
+        setOrderedFriends(sortedFriends)
 
-    // const handleDelete = (friend) => {
-    //     alert
-    // }
+    }, [friends])
 
     return (
         <div className="fiends-list-container">
 
             <h4 id="friends-list-title">FRIENDS</h4>
             <div className="friends-list">
-                {Object.values(friends).map(friend => (
+                {orderedFriends.map(friend => (
                     <div key={friend.id} className="friends-list-ele">
-                        <li >{friend.name}</li>
+                        <div onClick={() => navigate(`/friend/${friend.id}`)}>{friend.name}</div>
                         <OpenModalButton
                         buttonText={<HiOutlineX />}
                         modalComponent={<DeleteFriendModal friend={friend} />}
--- frontend/src/components/modals/AddFriendModal/AddFriendModal.jsx
+++ frontend/src/components/modals/AddFriendModal/AddFriendModal.jsx
@@ -1,5 +1,76 @@
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { thunkAddFriend } from "../../../redux/friends";
+import { useModal } from "../../../context/Modal";
+import { useNavigate } from "react-router-dom";
+import './AddFriendModal.css'
+
 function AddFriendModal() {
-    return <p>Add Friend Modal</p>
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const { closeModal } = useModal();
+    const [email, setEmail] = useState('')
+    const [suggestedFriends, setSuggestedFriends] = useState([])
+    const [friendSelected, setFriendSelected] = useState('')
+    // const currFriends = useSelector(state => state.friends)
+    const allUsers = useSelector(state => state.userEmails)
+
+    useEffect(() => {
+        // need to remove all current friends from this list
+        // const filteredUsers = Object.values(Object.values(allUsers)).map(user => !Object.values(Object.values(currFriends)).includes(user))
+        // console.log('filtered', filteredUsers)
+        const suggested = Object.values(allUsers).filter(user => user.email.startsWith(email))
+        if (email) {
+            setSuggestedFriends(suggested.slice(0, 5))
+            setFriendSelected('')
+        } else {
+            setSuggestedFriends([])
+        }
+
+    }, [email])
+
+    const selectingUser = (friend) => {
+        setFriendSelected(friend)
+        setSuggestedFriends([])
+        setEmail('')
+    }
+
+    const handleAddFriend = async (e) => {
+        e.preventDefault();
+
+        await dispatch(thunkAddFriend(friendSelected.email));
+
+        closeModal();
+        navigate(`friend/${friendSelected.id}`)
+
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleAddFriend}>
+                <div className="add-friend-input">
+                    <label>Add a Friend</label>
+                    <input
+                        type='text'
+                        value={email}
+                        placeholder="Enter an email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        >
+                    </input>
+                </div>
+                {suggestedFriends.map(friend => (
+                    <div key={friend.id}>
+                        <div className='list-user-email-item' onClick={() => selectingUser(friend)} >{friend.email}</div>
+                    </div>
+                ))}
+                <div className='chosen-user-container' hidden={!friendSelected}>
+                    <img className='user-profile-image' src={friendSelected.profileImage} hidden={!friendSelected}/>
+                    <div hidden={!friendSelected} >add {friendSelected.name} as a friend?</div>
+                </div>
+                <button type="submit">Add Friend</button>
+            </form>
+        </div>
+    )
 }
 
 export default AddFriendModal;
