3af5b4d6638846bd4799fb3caaa573c2371e50a3
Aubrie <aubriewoodbine@yahoo.com>
2024-05-22 22:04:12 -0500

    added friends list

--- frontend/src/components/FriendsList/FriendsList.jsx
+++ frontend/src/components/FriendsList/FriendsList.jsx
@@ -1,5 +1,25 @@
+import { useSelector } from "react-redux";
+import './friendsList.css'
+
+
 function FriendsList() {
-    return <p>Friends List</p>
+    const friends = useSelector(state => state.friends)
+
+    return (
+        <div className="fiends-list-container">
+            <h3>FRIENDS</h3>
+            <div className="friends-list">
+                {Object.values(friends).map(friend => (
+                    <div key={friend.id} className="friends-list-ele">
+                        <li >{friend.name}</li>
+                        <div>X</div>
+                    </div>
+                ))}
+            </div>
+            <button>INVITE A FRIEND</button>
+        </div>
+
+    )
 }
 
 export default FriendsList;
