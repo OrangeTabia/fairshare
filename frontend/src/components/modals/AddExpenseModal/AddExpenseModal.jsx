41eb4ca36f98aebd7dec0d4df4b186dff664d9a6
Tabia Ye <tabiaye@gmail.com>
2024-05-22 20:45:27 -0700

    started middle modal

--- frontend/src/components/modals/AddExpenseModal/AddExpenseModal.jsx
+++ frontend/src/components/modals/AddExpenseModal/AddExpenseModal.jsx
@@ -1,5 +1,16 @@
+import { useModal } from "../../../context/Modal";
+import OpenModalButton from "../OpenModalButton/OpenModalButton";
+
 function AddExpenseModal() {
-    return <p>Add Expense Modal</p>
+    return (
+        <>
+            <h1></h1>
+            <form>
+
+            </form>
+            <p>Add Expense Modal</p>
+        </>
+    )
 }
 
 export default AddExpenseModal;
