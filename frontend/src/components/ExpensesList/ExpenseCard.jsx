6aab9e665297131281e177d520dd0a4e140adb2d
Jeramie <forbesjeramie@gmail.com>
2024-05-23 19:36:26 -0400

    delete expense working

--- frontend/src/components/ExpensesList/ExpenseCard.jsx
+++ frontend/src/components/ExpensesList/ExpenseCard.jsx
@@ -1,16 +1,25 @@
 import OpenModalButton from "../modals/OpenModalButton/OpenModalButton";
 import EditExpenseModal from "../modals/EditExpenseModal";
-import './ExpenseCard.css'
+import "./ExpenseCard.css";
+
+import { HiOutlineX } from "react-icons/hi";
+import DeleteFriendsExpenseModal from "../modals/DeleteFriendsExpenseModal";
 
 //className={`expense-card ${expense.type}-expense`}
 
 function ExpenseCard({ expense }) {
   return (
     <div className="expense-details-card">
-
       <span>{expense.description}</span>
       <span>{expense.amount}</span>
       <span>{expense.expenseDate}</span>
+
+      <OpenModalButton
+        id="delete-expense-modal-button"
+        buttonText={<HiOutlineX />}
+        modalComponent={<DeleteFriendsExpenseModal expense={expense} />}
+      />
+
       <OpenModalButton
         id="edit-expense-modal-button"
         className="modal-button"
@@ -18,7 +27,7 @@ function ExpenseCard({ expense }) {
         modalComponent={<EditExpenseModal expense={expense} />}
       />
     </div>
-  )
+  );
 }
 
 export default ExpenseCard;
