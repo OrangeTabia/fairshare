eba3c49c13b9c9387f5a4a47ebd0dcf9eba6fc2f
Ethan Harrasser <ethan.harrasser@gmail.com>
2024-05-24 13:00:00 -0700

    navigation and profile dropdown reworked, z-index issues solved

--- frontend/src/components/Navigation/LoggedInNavigation.jsx
+++ frontend/src/components/Navigation/LoggedInNavigation.jsx
@@ -1,27 +1,18 @@
 import { NavLink } from "react-router-dom";
-import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 
 function LoggedInNavigation() {
   return (
-    <nav className="nav-header">
-      {/* <a href="/api/friends">
-        <span className="animated-text-underline">friends</span>
-      </a>
-      <a href="/api/friends_expenses">
-        <span className="animated-text-underline">expenses</span>
-      </a>
-      <a href="/api/payments">
-        <span className="animated-text-underline">payments</span>
-      </a> */}
-      <NavLink to="/">
-        <span className="animated-text-underline">Dashboard</span>
-      </NavLink>
-      <NavLink to="/expenses">
-        <span className="animated-text-underline">Expenses</span>
-      </NavLink>
-      <ProfileButton />
-    </nav>
+    <>
+      <nav className="nav-header">
+        <NavLink to="/">
+          <span className="animated-text-underline">Dashboard</span>
+        </NavLink>
+        <NavLink to="/expenses">
+          <span className="animated-text-underline">Expenses</span>
+        </NavLink>
+      </nav>
+    </>
   );
 }
 
