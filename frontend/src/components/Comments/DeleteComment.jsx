0a4f20152f8ca0f02d896cef2337928eb2d0f7fd
Tabia Ye <tabiaye@gmail.com>
2024-05-24 08:35:55 -0700

    minor tweaks for comments

--- frontend/src/components/Comments/DeleteComment.jsx
+++ frontend/src/components/Comments/DeleteComment.jsx
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { useModal } from "../../context/Modal";
 import { thunkDeleteComment } from "../../redux/friends_expenses";
@@ -6,11 +6,13 @@ import { thunkDeleteComment } from "../../redux/friends_expenses";
 function DeleteComment({ comments }) {
   const dispatch = useDispatch();
   const { closeModal } = useModal();
+  const commentsArray = Object.values(comments);
+  console.log("COMMENT ID", commentsArray)
 
 
   const deleteComment = async (e) => {
     e.preventDefault();
-    await dispatch(thunkDeleteComment(comments.comment));
+    await dispatch(thunkDeleteComment(commentId));
     closeModal();
   };
 
