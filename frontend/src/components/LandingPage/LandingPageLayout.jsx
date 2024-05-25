80a072b4295940349b91c63045a748005c4dc687
Aubrie <aubriewoodbine@yahoo.com>
2024-05-24 19:27:15 -0500

    basic landing page

--- frontend/src/components/LandingPage/LandingPageLayout.jsx
+++ frontend/src/components/LandingPage/LandingPageLayout.jsx
@@ -3,10 +3,16 @@ import './LandingPage.css'
 function LandingPageLayout () {
     return (
         <div id='landingPage-main-container'>
-            <div>
-                <h1>You want to share?</h1>
-                <h1>We make it fair!</h1>
+            <div className='elements-landing-container'>
+                <div className='landingPage-moto-container'>
+                    <h1 className='first-mot-line'>You want to share?</h1>
+                    <h1 className='second-mot-line'>We make it fair!</h1>
+                </div>
+                <div className='landingPage-image-container'>
+                    <img className='landingPage-image' src='https://static.vecteezy.com/system/resources/previews/005/337/799/non_2x/icon-image-not-found-free-vector.jpg' />
+                </div>
             </div>
+
 
         </div>
     )
