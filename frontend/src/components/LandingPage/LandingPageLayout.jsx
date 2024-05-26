ec1f4434e4147caa9d0e19b466098c4b24e3b9dd
Tabia Ye <tabiaye@gmail.com>
2024-05-25 21:47:25 -0700

    added design to landing page

--- frontend/src/components/LandingPage/LandingPageLayout.jsx
+++ frontend/src/components/LandingPage/LandingPageLayout.jsx
@@ -1,15 +1,29 @@
 import './LandingPage.css'; 
 import LandingPageImg from '../../../images/landing-page-img.png';
+import SignupFormModal from '../modals/SignupFormModal';
+import OpenModalButton from '../modals/OpenModalButton';
+import { useModal } from '../../context/Modal'; 
 
 function LandingPageLayout () {
+    const { closeModal } = useModal();
     return (
         <div id='landingPage-main-container'>
             <div className='elements-landing-container'>
                 <div className='landingPage-moto-container'>
-                    <h1 className='first-mot-line'>You want to share?</h1>
+                    <h1 className='first-mot-line'>You want to $hare?</h1>
                     <h1 className='second-mot-line'>We make it fair!</h1>
-                    <p>Keep track of your shared expenses and balances with housemates, trips, groups, friends, and family.</p>
-                    <button>Sign up</button>
+                    <div id="landing-sentence">
+                        <p>Keep track of your shared expenses and balances with housemates, trips, groups, friends, and family.</p>
+                    </div>
+                    <div>
+                        <OpenModalButton
+                            className="big-signup-button"
+                            buttonText="Sign Up"
+                            onItemClick={closeModal}
+                            modalComponent={<SignupFormModal />}
+                        />
+                    </div>
+
                 </div>
                 <div className='landingPage-image-container'>
                     <img className='landingPage-image' src={LandingPageImg} />
