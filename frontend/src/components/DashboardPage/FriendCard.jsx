8ce70c62c0b95096bd53279cec4fc3784d5eb541
Jeramie <forbesjeramie@gmail.com>
2024-05-25 23:04:34 -0400

    friends that owe is a link to friends

--- frontend/src/components/DashboardPage/FriendCard.jsx
+++ frontend/src/components/DashboardPage/FriendCard.jsx
@@ -1,29 +1,35 @@
+import { useNavigate } from "react-router-dom";
 import { centsToUSD } from "../../utils/formatters";
 import "./FriendCard.css";
 
 function FriendCard({ friend }) {
-    const netAmountClass = (friend.netAmount > 0)
-        ? "net-amount positive"
-        : "net-amount negative";
-    const owesText = (friend.netAmount > 0)
-        ? "owes you "
-        : "you owe ";
-    const netAmountText = (friend.netAmount > 0)
-        ? centsToUSD(friend.netAmount)
-        : centsToUSD(friend.netAmount * -1);
+  const navigate = useNavigate();
 
-    return <li className="friend-card">
-        <div className="friend-info">
-            <img src={friend.profileImage} className="friend-profile-image" />
-            <div className="friend-info-text">
-                <h3 className="friend-name">{friend.name}</h3>
-                <p className={netAmountClass}>
-                    {owesText}
-                    <span>{netAmountText}</span>
-                </p>
-            </div>
+  const netAmountClass =
+    friend.netAmount > 0 ? "net-amount positive" : "net-amount negative";
+  const owesText = friend.netAmount > 0 ? "owes you " : "you owe ";
+  const netAmountText =
+    friend.netAmount > 0
+      ? centsToUSD(friend.netAmount)
+      : centsToUSD(friend.netAmount * -1);
+
+  return (
+    <li
+      className="friend-card"
+      onClick={() => navigate(`/friend/${friend.id}`)}
+    >
+      <div className="friend-info">
+        <img src={friend.profileImage} className="friend-profile-image" />
+        <div className="friend-info-text">
+          <h3 className="friend-name">{friend.name}</h3>
+          <p className={netAmountClass}>
+            {owesText}
+            <span>{netAmountText}</span>
+          </p>
         </div>
+      </div>
     </li>
+  );
 }
 
 export default FriendCard;
