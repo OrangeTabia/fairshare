66e583db73dd3cd49d848d61494df508bd535675
Tabia Ye <tabiaye@gmail.com>
2024-05-26 12:00:08 -0700

    landing page edits

--- frontend/src/components/LandingPage/LandingPageLayout.jsx
+++ frontend/src/components/LandingPage/LandingPageLayout.jsx
@@ -13,7 +13,7 @@ function LandingPageLayout () {
     const { closeModal } = useModal();
     return (
         <div id='landingPage-main-container'>
-            <div className='elements-landing-container'>
+            <div className='elements-landing-container1'>
                 <div className='landingPage-moto-container'>
                     <div id="landing-mottos">
                         <h1 className='first-mot-line'>You want to $hare?</h1>
@@ -42,8 +42,64 @@ function LandingPageLayout () {
                     <img className='landingPage-image' src={LandingPageImg} />
                 </div>
             </div>
+            <div className='elements-landing-container2'>
+                <div className='landingPage-moto-container'>
+                    <div id="landing-mottos">
+                        <h1 className='first-mot-line'>You want to $hare?</h1>
+                        <h1 className='second-mot-line'>We make it fair!</h1>
+                    </div>
+                    <div id="landing-icons">
+                        <img className="landing-icon-images" src={HousematesIcon} alt="house"></img>
+                        <img className="landing-icon-images" src={TravelIcon} alt="plane"></img>
+                        <img className="landing-icon-images" src={GroupsIcon} alt="group"></img>
+                        <img className="landing-icon-images" src={Friends} alt="friends"></img>
+                    </div>
+                    <div id="landing-sentence">
+                        <span>Keep track of your shared expenses and balances <br></br>with <span>housemates</span>, <span>trips</span>, <span>groups</span>, <span>friends</span>, and <span>family</span>.</span>
+                    </div>
+                    <div>
+                        <OpenModalButton
+                            className="big-signup-button"
+                            buttonText="Sign Up"
+                            onItemClick={closeModal}
+                            modalComponent={<SignupFormModal />}
+                        />
+                    </div>
 
+                </div>
+                <div className='landingPage-image-container'>
+                    <img className='landingPage-image' src={LandingPageImg} />
+                </div>
+            </div>
+            <div className='elements-landing-container3'>
+                <div className='landingPage-moto-container'>
+                    <div id="landing-mottos">
+                        <h1 className='first-mot-line'>You want to $hare?</h1>
+                        <h1 className='second-mot-line'>We make it fair!</h1>
+                    </div>
+                    <div id="landing-icons">
+                        <img className="landing-icon-images" src={HousematesIcon} alt="house"></img>
+                        <img className="landing-icon-images" src={TravelIcon} alt="plane"></img>
+                        <img className="landing-icon-images" src={GroupsIcon} alt="group"></img>
+                        <img className="landing-icon-images" src={Friends} alt="friends"></img>
+                    </div>
+                    <div id="landing-sentence">
+                        <span>Keep track of your shared expenses and balances <br></br>with <span>housemates</span>, <span>trips</span>, <span>groups</span>, <span>friends</span>, and <span>family</span>.</span>
+                    </div>
+                    <div>
+                        <OpenModalButton
+                            className="big-signup-button"
+                            buttonText="Sign Up"
+                            onItemClick={closeModal}
+                            modalComponent={<SignupFormModal />}
+                        />
+                    </div>
 
+                </div>
+                <div className='landingPage-image-container'>
+                    <img className='landingPage-image' src={LandingPageImg} />
+                </div>
+            </div>
         </div>
     )
 }
